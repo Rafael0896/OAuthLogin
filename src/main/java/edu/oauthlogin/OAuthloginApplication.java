661487b11635a0eee4895bfd1dd84bdaa024cdae
Rafael0896 <realvarez54@misena.edu.co>
2025-06-21 07:49:38 -0500

    first commit

---
+++ src/main/java/edu/oauthlogin/OAuthloginApplication.java
@@ -0,0 +1,13 @@
+package edu.oauthlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthloginApplication.class, args);
+	}
+
+}
